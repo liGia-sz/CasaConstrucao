76470fb82e61b8efe70db4d764aa48b683894856
Ligia <s.morais.ligia@gmal.com>
2025-05-28 08:04:23 -0300

    Página História concluída

--- CasaMateriaisDeConstrucao/obj/Debug/net8.0/CasaMateriaisDeConstrucao.AssemblyInfo.cs
+++ CasaMateriaisDeConstrucao/obj/Debug/net8.0/CasaMateriaisDeConstrucao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CasaMateriaisDeConstrucao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92acf5258777f1323af38d4c51ee873e18b7380e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f8e586720211ea92cba698d4faff486e4d75758")]
 [assembly: System.Reflection.AssemblyProductAttribute("CasaMateriaisDeConstrucao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CasaMateriaisDeConstrucao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
